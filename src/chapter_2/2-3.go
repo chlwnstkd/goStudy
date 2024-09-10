77da0c3e3c9d65097e37a7c030a65ba241d59b20
poly <jun1234755@naver.com>
2024-09-10 11:27:22 +0900

    Go

---
+++ src/chapter_2/2-3.go
@@ -0,0 +1,10 @@
+package main
+
+import {
+	"fmt"
+	_"time" // 사용하지 않는 패키지로 인한 컴파일 에러 방지
+}
+
+func main) {
+	fmt.Println("Hello Word!")
+}
---
+++ src/chapter_4/4-4.go
@@ -0,0 +1,14 @@
+// 4-4
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	a := [5]int{32, 29, 78, 16, 8}
+
+	for value := range a {
+		fmt.Println(i, value)
+	}
+}
